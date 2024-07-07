fd8db733f755deeaa9a80c60f5b20fb01cfbc11b
Taras Chepil <agent000666@gmail.com>
2024-07-07 17:49:40 +0300

    2

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,17 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const list = document.querySelector(`.gallery`);
+
+const markup = images
+  .map((image) => `<li class="js-image"><img src="${image.url}" alt="${image.alt}"></li>
+`)
+  .join("");
+
+list.insertAdjacentHTML(`afterbegin`, markup);
+
+list.classList.add('gallery');
+list.firstChild.classList.add('js-image');
+
+console.log(list);
